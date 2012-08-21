687ef7b942ed3a8c3cea64708bc5a19da7797722
Petr Prokop <pitrowech@gmail.com>
2012-08-21 17:36:30 +0400

    Initial Commit

---
+++ FoursquareExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FoursquareExample
+//
+//  Created by macair on 21.08.12.
+//  Copyright (c) 2012 macair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FoursquareExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FoursquareExample
+//
+//  Created by macair on 21.08.12.
+//  Copyright (c) 2012 macair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
